e5573b5b8f70d180c05b286bd471db0a37820e15
Rikikangsc2-eng <nueapi11@gmail.com>
2025-03-18 11:07:24 +0700

    update index.js

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.get('/produk', async (req, res) => {
     url: targetUrl,
     _rsc: '1iwkq',
     device: 'phone',
-    dimension: '480x440',
+    dimension: '720x1280',
     format: 'jpg',
     cacheLimit: '14',
     delay: '1000',
