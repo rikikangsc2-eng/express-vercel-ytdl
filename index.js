ef0a7eb3c0f8e8a348146f7a546d03156bb6c9fd
Rikikangsc2-eng <nueapi11@gmail.com>
2025-03-20 20:30:25 +0700

    update index.js

--- index.js
+++ index.js
@@ -14,50 +14,6 @@ const voice = new ElevenLabs({
   voiceId: "kuOK5r8Woz6lkWaMr8kx"
 });
 
-app.get('/soundoftext', async (req, res) => {
-  const text = req.query.text;
-  if (!text) {
-    return res.status(400).send('Parameter "text" is required.');
-  }
-
-  try {
-    const response = await axios({
-      method: 'post',
-      url: 'https://api.soundoftext.com/sounds',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': '*/*',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7',
-        'Origin': 'https://soundoftext.com',
-        'Referer': 'https://soundoftext.com/',
-        'Sec-Ch-Ua': '"Not A(Brand";v="8", "Chromium";v="132"',
-        'Sec-Ch-Ua-Mobile': '?1',
-        'Sec-Ch-Ua-Platform': '"Android"',
-        'Sec-Fetch-Dest': 'empty',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Site': 'same-site',
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
-      },
-      data: {
-        engine: 'Google',
-        data: {
-          text: text,
-          voice: 'id-ID',
-        },
-      },
-    });
-    response.data.id = "https://files.soundoftext.com/"+response.data.id+".mp3"
-    res.json(response.data.id);
-  } catch (error) {
-    console.error('Error calling Sound of Text API:', error);
-    if (error.response) {
-      res.status(error.response.status).send(error.response.data);
-    } else {
-      res.status(500).send('Internal Server Error');
-    }
-  }
-});
 
 
 app.get('/brats', async (req, res) => {
