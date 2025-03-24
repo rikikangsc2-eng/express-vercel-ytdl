91415083977735598f84e69d6d6479709b6b5d27
Rikikangsc2-eng <nueapi11@gmail.com>
2025-03-24 14:57:23 +0700

    update tes.js

--- tes.js
+++ tes.js
@@ -9,13 +9,13 @@ module.exports = async (req, res) => {
     
     $('tr').each((_, el) => {
       const rank = $(el).find('td:nth-child(1)').text().trim();
-      const img = $(el).find('td:nth-child(2) img').attr('src');
+      const img = $(el).find('td:nth-child(2) img').attr('data-src') || $(el).find('td:nth-child(2) img').attr('src');
       const title = $(el).find('td:nth-child(3) strong').text().trim();
       const artist = $(el).find('td:nth-child(3) span').text().trim();
       const youtube = $(el).find('td:nth-child(4) a').attr('href');
       const spotify = $(el).find('td:nth-child(5) a').attr('href');
       
-      if (rank && title) {
+      if (rank && title && img.startsWith('http')) {
         songs.push({ rank, img, title, artist, youtube, spotify });
       }
     });
