abd536592861beb55c152ce952a44f798d0f2f25
Rikikangsc2-eng <nueapi11@gmail.com>
2025-03-26 07:44:50 +0700

    update tes.js

--- tes.js
+++ tes.js
@@ -1,44 +1,57 @@
 const axios = require("axios");
 const AWS = require("aws-sdk");
+const crypto = require("crypto");
 
-AWS.config.credentials = new AWS.EnvironmentCredentials("AWS");
+AWS.config.update({ region: "us-east-1" });
 
-async function getAWSCredentials() {
-  const sts = new AWS.STS();
-  const data = await sts.getSessionToken().promise();
-  return {
-    accessKeyId: data.Credentials.AccessKeyId,
-    secretAccessKey: data.Credentials.SecretAccessKey,
-    sessionToken: data.Credentials.SessionToken,
-  };
-}
-
-async function generateSignature(credentials, requestDate) {
-  const crypto = require("crypto");
-  const region = "us-east-1";
-  const service = "execute-api";
-  const credentialScope = `${requestDate}/${region}/${service}/aws4_request`;
-  const stringToSign = `AWS4-HMAC-SHA256
-${requestDate}T000000Z
-${credentialScope}
-${crypto.createHash("sha256").update("").digest("hex")}`;
-  
-  function getSignatureKey(key, dateStamp, regionName, serviceName) {
-    const kDate = crypto.createHmac("sha256", "AWS4" + key).update(dateStamp).digest();
-    const kRegion = crypto.createHmac("sha256", kDate).update(regionName).digest();
-    const kService = crypto.createHmac("sha256", kRegion).update(serviceName).digest();
-    return crypto.createHmac("sha256", kService).update("aws4_request").digest();
+async function generateSignature(credentials, requestDate, canonicalRequest) {
+  function hmac(key, data) {
+    return crypto.createHmac("sha256", key).update(data).digest();
   }
   
-  const signingKey = getSignatureKey(credentials.secretAccessKey, requestDate, region, service);
+  function getSignatureKey(key, dateStamp, regionName, serviceName) {
+    const kDate = hmac("AWS4" + key, dateStamp);
+    const kRegion = hmac(kDate, regionName);
+    const kService = hmac(kRegion, serviceName);
+    return hmac(kService, "aws4_request");
+  }
+  
+  const stringToSign = `AWS4-HMAC-SHA256
+${requestDate}T000000Z
+${requestDate}/us-east-1/execute-api/aws4_request
+${crypto.createHash("sha256").update(canonicalRequest).digest("hex")}`;
+  
+  const signingKey = getSignatureKey(credentials.secretAccessKey, requestDate, "us-east-1", "execute-api");
   return crypto.createHmac("sha256", signingKey).update(stringToSign).digest("hex");
 }
 
 module.exports = async (req, res) => {
   try {
-    const credentials = await getAWSCredentials();
+    const credentials = AWS.config.credentials;
+    
+    if (!credentials || !credentials.accessKeyId || !credentials.secretAccessKey) {
+      throw new Error("AWS credentials are not available.");
+    }
+    
     const requestDate = new Date().toISOString().split("T")[0].replace(/-/g, "");
-    const signature = await generateSignature(credentials, requestDate);
+    const host = "moxu0s1jnk.execute-api.us-east-1.amazonaws.com";
+    const endpoint = `https://${host}/prod-wpm/tts`;
+    
+    const queryParams = `e=user%40naturalreaders.com&l=0&r=161&s=0&v=ms&vn=10.4.14.3&sm=true&lo=id-ID`;
+    const payload = JSON.stringify({ t: "Aku gak tau sumpah" });
+    
+    const canonicalRequest = `POST
+/prod-wpm/tts
+${queryParams}
+content-type:application/json; charset=UTF-8
+host:${host}
+x-amz-date:${requestDate}T000000Z
+x-amz-security-token:${credentials.sessionToken}
+
+content-type;host;x-amz-date;x-amz-security-token
+${crypto.createHash("sha256").update(payload).digest("hex")}`;
+    
+    const signature = await generateSignature(credentials, requestDate, canonicalRequest);
     
     const headers = {
       "Content-Type": "application/json; charset=UTF-8",
@@ -47,20 +60,20 @@ module.exports = async (req, res) => {
       Authorization: `AWS4-HMAC-SHA256 Credential=${credentials.accessKeyId}/${requestDate}/us-east-1/execute-api/aws4_request, SignedHeaders=content-type;host;x-amz-date;x-amz-security-token, Signature=${signature}`,
     };
     
-    const payload = { t: "Aku gak tau sumpah" };
-    const queryParams = {
-      e: "user@naturalreaders.com",
-      l: "0",
-      r: "161",
-      s: "0",
-      v: "ms",
-      vn: "10.4.14.3",
-      sm: "true",
-      lo: "id-ID",
-    };
-    
-    const url = "https://moxu0s1jnk.execute-api.us-east-1.amazonaws.com/prod-wpm/tts";
-    const response = await axios.post(url, payload, { headers, params: queryParams, responseType: "arraybuffer" });
+    const response = await axios.post(endpoint, payload, {
+      headers,
+      params: {
+        e: "user@naturalreaders.com",
+        l: "0",
+        r: "161",
+        s: "0",
+        v: "ms",
+        vn: "10.4.14.3",
+        sm: "true",
+        lo: "id-ID",
+      },
+      responseType: "arraybuffer",
+    });
     
     res.setHeader("Content-Type", "audio/mpeg");
     res.send(response.data);
