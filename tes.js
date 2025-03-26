a990bd8dd89633e5c24e968a8fa932fc4686b5e3
Rikikangsc2-eng <nueapi11@gmail.com>
2025-03-27 06:32:49 +0700

    update tes.js

--- tes.js
+++ tes.js
@@ -1,35 +1,46 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-
+const axios = require("axios");
+const crypto = require("crypto");
 module.exports = async (req, res) => {
+  const task_id = crypto.randomUUID();
+  const baseURL = "https://magichour.ai";
   try {
-    const response = await axios.get('https://myanimelist.net/topanime.php?type=bypopularity', {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 10; RMX2185 Build/QP1A.190711.020) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.6998.40 Mobile Safari/537.36',
-        'Referer': 'https://myanimelist.net/topanime.php?type=bypopularity'
+    await axios.post(
+      `${baseURL}/api/free-tools/v1/ai-image-generator`,
+      {
+        prompt: "Girl sitting on bed",
+        orientation: "square",
+        tool: "ai-anime-generator",
+        task_id: task_id
+      },
+      {
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+          "x-timezone-offset": "-420",
+          "User-Agent":
+            "Mozilla/5.0 (Linux; Android 10; RMX2185 Build/QP1A.190711.020) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.6998.40 Mobile Safari/537.36"
+        }
       }
-    });
-    
-    const $ = cheerio.load(response.data);
-    const result = [];
-    
-    $('.information').each((_, el) => {
-      const rank = $(el).find('.rank .text').text().trim();
-      const title = $(el).find('.title').text().trim();
-      const type = $(el).find('.misc .type').text().trim();
-      const score = $(el).find('.score').text().trim();
-      const members = $(el).find('.member').text().trim();
-      const link = $(el).next('.thumb').attr('href') || '';
-      
-      const imgDiv = $(el).parent().next('.tile-unit');
-      const image = imgDiv.data('bg') || '';
-      
-      if (rank && title) {
-        result.push({ rank, title, type, score, members, link, image });
+    );
+    const poll = async () => {
+      const statusResponse = await axios.get(
+        `${baseURL}/api/free-tools/v1/ai-image-generator/${task_id}/status`,
+        {
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "x-timezone-offset": "-420",
+            "User-Agent":
+              "Mozilla/5.0 (Linux; Android 10; RMX2185 Build/QP1A.190711.020) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.6998.40 Mobile Safari/537.36"
+          }
+        }
+      );
+      if (statusResponse.data.status === "SUCCESS") {
+        return statusResponse.data;
       }
-    });
-    
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      return await poll();
+    };
+    const result = await poll();
     res.json(result);
   } catch (error) {
     res.status(500).json({ error: error.message });
