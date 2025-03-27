9dc7a607783cbc269c0b9f655c6a8055ed0f7c19
Rikikangsc2-eng <nueapi11@gmail.com>
2025-03-27 18:07:14 +0700

    update tes.js

--- tes.js
+++ tes.js
@@ -3,23 +3,20 @@ const cheerio = require("cheerio");
 
 module.exports = async (req, res) => {
   try {
-    // URL utama untuk mendapatkan token
     const url = "https://pdfcrowd.com/html-to-image/#convert_by_input";
     const apiUrl = "https://pdfcrowd.com/form/json/convert/html/v2/";
     
-    // Step 1: Ambil halaman utama untuk mendapatkan CSRF token
     const initialResponse = await axios.get(url, {
       headers: {
         "User-Agent": "Mozilla/5.0 (Linux; Android 10; RMX2185 Build/QP1A.190711.020) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.6998.40 Mobile Safari/537.36"
       }
     });
     
-    // Ambil cookies dari response header
     const cookies = initialResponse.headers["set-cookie"];
     
-    // Parsing HTML untuk mendapatkan CSRF token
     const $ = cheerio.load(initialResponse.data);
     const csrftoken = $("input[name='csrfmiddlewaretoken']").val();
+    const ias = Math.floor(Math.random() * 1e10).toString();
     
     if (!csrftoken) {
       return res.status(500).json({ error: "CSRF token not found" });
@@ -27,7 +24,6 @@ module.exports = async (req, res) => {
     
     console.log("CSRF Token:", csrftoken);
     
-    // Step 2: Kirim permintaan POST ke API PdfCrowd
     const postData = new URLSearchParams({
       csrfmiddlewaretoken: csrftoken,
       conversion_source: "content",
@@ -40,7 +36,7 @@ module.exports = async (req, res) => {
       img_main_content_only: "off",
       img_readability_enhancements: "on",
       _dontcare: "",
-      ias: "3875317856"
+      ias: ias
     });
     
     const headers = {
@@ -53,7 +49,6 @@ module.exports = async (req, res) => {
     
     const screenshotResponse = await axios.post(apiUrl, postData.toString(), { headers });
     
-    // Step 3: Kirimkan hasil ke klien
     if (screenshotResponse.data && screenshotResponse.data.status === "ok") {
       return res.json({
         success: true,
