6b97f2ebe2fe9c4f076da9fd8c2a6cb941f223bb
Rikikangsc2-eng <nueapi11@gmail.com>
2025-03-29 08:50:33 +0700

    update tes.js

--- tes.js
+++ tes.js
@@ -2,10 +2,15 @@ const axios = require('axios'); const cheerio = require('cheerio');
 
 module.exports = async (req, res) => { try { const url = 'https://id.m.wikipedia.org/wiki/Special:Random'; const { data } = await axios.get(url); const $ = cheerio.load(data);
 
-const title = $('span.mw-page-title-main').text().trim();
-    const paragraph = $('p').first().text().trim();
+const title = $('span.mw-page-title-main').text().trim() || "";
+    const description = $('p').first().text().trim() || "";
+    const image = $('figure.mw-default-size img').attr('src');
     
-    res.json({ title, paragraph });
+    res.json({
+        title,
+        description,
+        image: image ? `https:${image}` : null
+    });
 } catch (error) {
     res.status(500).json({ error: 'Gagal mengambil data' });
 }
