1d483ad5b259e498fd75446e9970945ac8a62800
Rikikangsc2-eng <nueapi11@gmail.com>
2025-03-29 22:55:32 +0700

    update tes.js

--- tes.js
+++ tes.js
@@ -1,25 +1,22 @@
-const axios = require('axios');
-const jsdom = require('jsdom');
-const { JSDOM } = jsdom;
+const axios = require('axios'); const { JSDOM } = require('jsdom');
 
-module.exports = async (req, res) => {
-  try {
-    const { data } = await axios.get('https://codebeautify.org/javascript-obfuscator', {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 10; RMX2185 Build/QP1A.190711.020) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.6998.40 Mobile Safari/537.36',
-        'Referer': 'https://codebeautify.org/javascript-obfuscator'
-      }
+module.exports = async (req, res) => { try { const { data } = await axios.get('https://codebeautify.org/javascript-obfuscator', { headers: { 'Content-Type': 'application/x-www-form-urlencoded', 'User-Agent': 'Mozilla/5.0 (Linux; Android 10; RMX2185 Build/QP1A.190711.020) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.6998.40 Mobile Safari/537.36', 'Referer': 'https://codebeautify.org/javascript-obfuscator' } });
+
+const dom = new JSDOM(data);
+    const document = dom.window.document;
+    const aceContents = document.querySelectorAll('.ace_content');
+    let kode = req.query.kode || "const contoh = 'contoh'";
+
+    aceContents.forEach(el => {
+        if (el.querySelector('.ace_placeholder')) {
+            el.innerHTML = `<div class="ace_line">${kode}</div>`;
+        }
     });
-    
-    const dom = new JSDOM(data);
-    const kode = req.query.kode || "const contoh = 'contoh'";
-    const elements = dom.window.document.querySelectorAll('.ace_content');
-    
-    elements.forEach(el => el.innerHTML = `<div class="ace_line">${kode}</div>`);
-    
+
     res.send(dom.serialize());
-  } catch (error) {
-    res.status(500).send('Terjadi kesalahan');
-  }
+} catch (error) {
+    res.status(500).send('Error fetching data');
+}
+
 };
+
