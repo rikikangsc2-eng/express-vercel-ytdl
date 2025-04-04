d83f16f1451ff8f02e37209a1f3edaf9a24190ba
Rikikangsc2-eng <nueapi11@gmail.com>
2025-04-04 19:38:28 +0700

    update tes.js

--- tes.js
+++ tes.js
@@ -1,5 +1,27 @@
-const axios = require('axios');
-const { JSDOM } = require('jsdom');
+const axios = require('axios'); const jsdom = require('jsdom'); const { JSDOM } = jsdom;
 
-module.exports = async (req, res) => { try { const response = await axios.post('https://y2meta.tube/id1/search/', new URLSearchParams({ query: 'https://youtu.be/-ktlIHSOOmk?si=fLKNk60e2dM4Dpdz' }).toString(), { headers: { 'Content-Type': 'application/x-www-form-urlencoded', 'User-Agent': 'Mozilla/5.0 (Linux; Android 10; RMX2185 Build/QP1A.190711.020) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.6998.135 Mobile Safari/537.36', 'Referer': 'https://y2meta.tube/id1/' } });
-    res.send(response.data); } catch (error) { res.json({ error: error.message }); } };
+module.exports = async (req, res) => { try { const url = 'https://y2meta.tube/id1/'; const headers = { 'Content-Type': 'application/x-www-form-urlencoded', 'User-Agent': 'Mozilla/5.0 (Linux; Android 10; RMX2185 Build/QP1A.190711.020) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.6998.135 Mobile Safari/537.36', 'Referer': url };
+
+const response = await axios.get(url, { headers });
+    const dom = new JSDOM(response.data);
+    const form = dom.window.document.querySelector('#search-form');
+    
+    if (!form) {
+        return res.json({ error: 'Form not found on the page' });
+    }
+    
+    const formAction = form.action;
+    const postUrl = new URL(formAction, url).href;
+    
+    const postData = new URLSearchParams();
+    postData.append('query', 'https://youtu.be/-ktlIHSOOmk?si=fLKNk60e2dM4Dpdz');
+    
+    const postResponse = await axios.post(postUrl, postData.toString(), { headers });
+    
+    res.send(postResponse.data);
+} catch (error) {
+    res.json({ error: error.message });
+}
+
+};
+
